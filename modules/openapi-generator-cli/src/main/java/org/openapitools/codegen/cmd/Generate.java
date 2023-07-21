3b6104ff94e805d039967f0da4bda4477dec4646
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2023-07-21 15:45:32 +0300

    fix: add Import

--- modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/cmd/Generate.java
+++ modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/cmd/Generate.java
@@ -26,6 +26,7 @@ import io.airlift.airline.Command;
 import io.airlift.airline.Option;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
